de67b049541aeb60988fed9db285593a08be0f2a
dlekdns08 <dlekdns08@gmail.com>
2026-05-12 09:15:06 +0900

    types.d.ts 파일 수정: 'matter-js' 모듈 선언 제거 및 Matter 네임스페이스 정리

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,12 +1,8 @@
-import 'matter-js';
-
-declare module 'matter-js' {
-  namespace Matter {
-    interface Body {
-      tier?: number;
-      _merged?: boolean;
-      _spawnT?: number;
-      _phaseActive?: boolean;
-    }
+declare namespace Matter {
+  interface Body {
+    tier?: number;
+    _merged?: boolean;
+    _spawnT?: number;
+    _phaseActive?: boolean;
   }
 }
